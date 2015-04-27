17364d368aac0e2987bb7f40e06ac4751f11dba5
Robert Bastian <robert@bastian.com>
2015-04-27 18:17:32 +0100

    minimum password length

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: {case_sensitive: false}, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
   before_save { self.email = email.downcase }
   has_secure_password
-  validates_confirmation_of :password
+  validates :password, length {minimum: 8}
   has_many :problems
   has_many :solutions
 end
