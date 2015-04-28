c7c752afa31afb414f6d39d9c9abd84780c31c4e
aiken <acairncross@gmail.com>
2015-04-28 19:58:37 +0100

    Basic problem creation

--- app/controllers/problem_controller.rb
+++ app/controllers/problem_controller.rb
@@ -7,4 +7,22 @@ class ProblemController < ApplicationController
 	def show
 		@problem = Problem.find(params[:id])
 	end
+
+	def new
+		@problem = Problem.new
+	end
+
+	def create
+		@problem = current_user.problems.build(problem_params)
+		# TODO: Clean up what happens on successful/unsuccessful saving
+		if @problem.valid?
+			@problem.save
+		end
+		redirect_to root_url
+	end
+
+	private
+		def problem_params
+			params.require(:problem).permit(:title, :text)
+		end
 end
