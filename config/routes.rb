e50e24a280e23fd59d3ec84cec2e92eac3090686
aiken <acairncross@gmail.com>
2015-04-30 18:49:49 +0100

    Add comment about nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,9 +14,15 @@ Rails.application.routes.draw do
   get '/logout' => 'sessions#destroy'
   post '/logout' => 'sessions#destroy'
 
+  # Routes like:
+  # /problems
+  # /problems/:id
+  # /problems/:problem_id/solutions
+  # Run 'bin/rake routes' for details
   resources :problems, only: [:index, :new, :create, :show] do
     resources :solutions, only: [:index, :new, :create]
   end
+  # Don't nest this one, because that would be overkill
   resources :solutions, only: [:show]
 
 end
