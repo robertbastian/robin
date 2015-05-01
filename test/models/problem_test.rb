adb2958971f834b8404ff3a6dbcc96be5be81fee
AGavin <alistair.gavin@some.ox.ac.uk>
2015-05-01 23:38:29 +0100

    Another test
    Should have been in previous commit

--- test/models/problem_test.rb
+++ test/models/problem_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class ProblemTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "problem is valid" do
+    assert problems(:problem1).valid? "Problem1 is not valid"
+  end
+  
+  test "winner is not the same as setter" do
+    assert problems(:problem1).valid? "Problem1 is not valid - test inconclusive"
+    problems(:problem1).winner = problems(:problem1).author
+	assert_not problems(:problem1).valid? "The setter cannot be the same as the winner"
+	# This validation has not yet been implemented
 end
