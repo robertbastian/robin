c0ebb3678da2560b1fcd85f2e1cc1193b8582afc
AGavin <alistair.gavin@some.ox.ac.uk>
2015-05-01 22:29:44 +0100

    Added simple tests
    I still can't find a way of running ruby in the terminal, which this
    requires. Going to 52.16.192.137:3000 does get me to the website, but
    the tests cannot be run from there. Can someone run them for me with $
    bin/rake test test/models/user_test.rb and see if they work

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "user is valid" do
+	assert users(:david).valid? "User should be valid"
+  end
 end
