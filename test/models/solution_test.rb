297fda00c2111948c7355d7bdeadfb0d72af4490
AGavin <alistair.gavin@some.ox.ac.uk>
2015-05-01 23:38:57 +0100

    Revert "More tests"
    This reverts commit 08bcc66990f80dfb9bc44857423961135265923e.

--- test/models/solution_test.rb
+++ test/models/solution_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class SolutionTest < ActiveSupport::TestCase
-  test "solution is valid" do
-	assert solutions(:fredSolution1).valid? "This solution should be vaid"
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
