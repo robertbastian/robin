8b03076455ca64d0a56ef376bd567fcde4b5bdf3
Robert Bastian <robert@bastian.com>
2015-05-02 19:39:39 +0100

    Fixing some stuff

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -2,5 +2,10 @@ class Problem < ActiveRecord::Base
   belongs_to :author, foreign_key: 'user_id', class_name: 'User'
   belongs_to :winner, foreign_key: 'winner_id', class_name: 'User'
   has_many :solutions
-  attr_default :points, 0
+	after_initialize :default_values
+
+  private
+  def default_values
+    self.score = 0
+  end
 end
