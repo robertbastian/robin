4b2555cc3e9b551bf34709fa5130f1dd0479f8f3
aiken <acairncross@gmail.com>
2015-05-02 15:03:06 +0100

    Comment out attr_default because it's not defined as a method (of ActiveRecord) and causes errors.
    Add 'end' in problem_test.
    
    Setup users in user_test instead of users.yml.

--- app/models/problem.rb
+++ app/models/problem.rb
@@ -2,5 +2,5 @@ class Problem < ActiveRecord::Base
   belongs_to :author, foreign_key: 'user_id', class_name: 'User'
   belongs_to :winner, foreign_key: 'winner_id', class_name: 'User'
   has_many :solutions
-  attr_default :points, 0
+  # attr_default :points, 0
 end
--- app/models/solution.rb
+++ app/models/solution.rb
@@ -1,5 +1,5 @@
 class Solution < ActiveRecord::Base
   belongs_to :user
   belongs_to :problem
-  attr_default :points, 0
+  # attr_default :points, 0
 end
