1d7d10d0f7ad51f91d6d8c0f01c0d3da3fc3bc30
Robert Bastian <robert@bastian.com>
2015-05-03 17:28:19 +0100

    Revert "user can set a problem if there is no most recent problem. Can't if the winner of the most recent problem hasn't been decided yet."
    This reverts commit d9e2cc6d5095065cb79ca39c79b1652b817fd890.

--- app/controllers/problems_controller.rb
+++ app/controllers/problems_controller.rb
@@ -10,27 +10,29 @@ class ProblemsController < ApplicationController
 	end
 
 	def new
-		@mrp = most_recent_problem
-		if !@mrp || (@mrp.winner && @mrp.winner == current_user && !active_problem?)
+		if most_recent_problem.winner == current_user && !active_problem?
 			@problem = Problem.new
 		else
-			if !@mrp.winner flash[:danger] = "Last problem doesn't have a winner yet"
-			else flash[:danger] = 'You shouldn\'t be here'
-			end
+			flash[:danger] = 'You shouldn\'t be here'
 			redirect_to root_url
 		end
 	end
 
 	def create
 		# Only the last winner can create a problem
-		@problem = current_user.problems.build(problem_params)
-		@problem.expiry = (params[:time] || 3).days.from_now
-		if @problem.save
-		# save returns a boolean that is true if problem is valid
-			redirect_to root_url
+		if most_recent_problem.winner == current_user
+			@problem = current_user.problems.build(problem_params)
+			@problem.expiry = (params[:time] || 3).days.from_now
+			if @problem.save
+			# save returns a boolean that is true if problem is valid
+				redirect_to root_url
+			else
+				flash[:danger] = @problem.errors.full_messages.first
+				render 'new'
+			end
 		else
-			flash[:danger] = @problem.errors.full_messages.first
-			render 'new'
+			flash[:danger] = 'You can\'t set a problem now'
+			redirect_to root_url
 		end
 	end
 
