97dfdef8eb016ee43372dd411a698833a4235917
Robert Bastian <robert@bastian.com>
2015-05-03 23:14:37 +0100

    even prettier navbar

--- app/controllers/solutions_controller.rb
+++ app/controllers/solutions_controller.rb
@@ -14,13 +14,21 @@ class SolutionsController < ApplicationController
 	end
 
 	def create
-		if most_recent_problem.id == params[:problem_id].to_i
+		if most_recent_problem.id != params[:problem_id].to_i
+			flash[:danger] = "You can't submit solutions for this problem anymore"
+		elsif most_recent_problem.solutions.any? {|solution| solution.user == current_user}
+			flash[:danger] = "You have already submitted a solution for this problem"
+		else
 			@solution = current_user.solutions.build(params.require(:solution).permit(:text))
 			@solution.problem = most_recent_problem
 			@solution.save
+			# all users submitted
+			if current_problem.solutions.length == User.size
+				current_problem.expiry = DateTime.now
+			end
+			flash[:notice] = 'Should now rate problem'
 			redirect_to most_recent_problem
 		else
-			flash[:danger] = "You can't submit solutions for this problem anymore"
 		end
 	end
 end
