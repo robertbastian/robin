8f010fd3e71a8c10bd8eec229aa8fe209d56b8d4
James Dai <james.dai.uk@googlemail.com>
2015-05-05 15:19:41 +0100

    Added a sample problem. Don't know if it works.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+james = User.create(name: "james", email: "james@ox.ac.uk", password: "testtest", password_confirmation: "testtest")
+User.create(:name => "rob", :email => "rob@ox.ac.uk", :password => 'testtest', :password_confirmation => 'testtest')
+#users = User.create([{name: "james", password: "dai", password_confirm: "dai"},
+	#{name: "robert", password: "bastian", password_confirm: "bastian"}])
+#problem1 =  Problem.create([{}])
+
+james.problems.build(title: "Functional Curried Ackermann", text: "The ackermann function is defined ack(0,y) = y+1, ack(x+1,0) = ack(x,1), ack (x+1,y+1) = ack(x, ack(x+1, y)).
+	Define f such that ack_c = foldn (succ, f) is a curried version of the ackermann function, i.e. ack(x,y) = ack_c x y. Succ is the natural number successor function, and foldn (e, g) n applies g to e n times.", expiry: 1.day.from_now)
