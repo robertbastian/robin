26a03eba5e556c326651d8b72e7d4efcf76fe31b
aiken <acairncross@gmail.com>
2015-05-06 21:12:19 +0100

    Hovis Best of Both

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -1,4 +1,10 @@
 class Solution < ActiveRecord::Base
   belongs_to :user
   belongs_to :problem
+  after_initialize :default_values
+
+  private
+    def default_values
+      self.score ||= 0
+    end
 end
