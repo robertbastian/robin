cf9eb3df0fc4040d4a7c1c5edb06dc53e22fa7b7
James Dai <james.dai.uk@googlemail.com>
2015-05-07 21:59:44 +0100

    mythos

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -2,7 +2,8 @@ module UserHelper
 	def question_master?
 		most_recent_problem && current_user == most_recent_problem.author && most_recent_problem.winner == nil
 	end
-
+  # cool, eventually we could parameterise this when we have more complex
+  # "score", and display different aspects of the score.
   def pointsFor user
     user.solutions.sum(:score) + user.problems.sum(:score)
   end
