0c60c3b3cbf2e7c1bd69cc6c48f4fec88c4044e6
Ubuntu <ubuntu@ip-172-31-32-38.eu-west-1.compute.internal>
2015-05-07 11:14:41 +0000

    another one!

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -14,7 +14,7 @@ class DashboardController < ApplicationController
 		@leaders = User.order(:score => :desc)
 		i = 0
 		my_pos = @leaders.find_index(current_user)
-		@start_pos = if my_pos == 0 then 0 elsif my_pos == User.count then User.count - 3 else my_pos - 1 end
+		@start_pos = if my_pos == 0 then 0 elsif my_pos == User.count - 1 then User.count - 3 else my_pos - 1 end
     @most_recent_problem = most_recent_problem
 	end
 end
